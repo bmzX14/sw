be35e288c2f6a41aaf9d7e2b18678d9027c2e2d6
jeong-an <aneve825@gmail.com>
2026-05-11 11:13:38 +0900

    add week2 post pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import Browse from "./pages/Browse";
+import PostForm from "./pages/PostForm";
+import PostDetail from "./pages/PostDetail";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/posts/new" element={<PostForm />} />
+        <Route path="/posts/:id" element={<PostDetail />} />
       </Routes>
     </BrowserRouter>
   );
